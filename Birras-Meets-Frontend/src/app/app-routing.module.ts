e799f9230b21bf15df0052202ca03b7a13bf59a6
alejoborsetti <75228768+alejoborsetti@users.noreply.github.com>
2021-02-19 12:07:49 -0300

    Create Birras-Meets-Frontend

---
+++ Birras-Meets-Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CalendarComponent } from './paginas/calendar/calendar.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { RegisterComponent } from './paginas/register/register.component';
+import { StockComponent } from './paginas/stock/stock.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'stock', component: StockComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ Birras-Meets-Frontend/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { RegisterComponent } from './paginas/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CalendarComponent } from './paginas/calendar/calendar.component';
+import { StockComponent } from './paginas/stock/stock.component';
+import { HeaderComponent } from './components/header/header.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    CalendarComponent,
+    StockComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule { }
