30955f6fc63eefc4affa51d70557fe4b609a3e40
alejoborsetti <75228768+alejoborsetti@users.noreply.github.com>
2021-02-23 09:47:31 -0300

    backend appointments

--- Birras-Meets-Backend/server.js
+++ Birras-Meets-Backend/server.js
@@ -27,3 +27,49 @@ router.get('/', (req, res) => {
 });
 app.use(router);
 app.listen(propierties.PORT, () => console.log(`Server runing on port ${propierties.PORT}`));
+
+// Appointments
+
+var createError = require('http-errors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const config = require('./appointments/config');
+const MongoClient = require('mongodb').MongoClient;
+
+var indexRouter = require('./appointments/routes/index');
+var usersRouter = require('./appointments/routes/users');
+
+MongoClient.connect(`mongodb://${config.dbHost}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(client => {
+    const db = client.db(config.dbName);
+    const collection = db.collection(config.dbCollection);
+    app.locals[config.dbCollection] = collection;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.use((req, res, next) => {
+  const collection = req.app.locals[config.dbCollection];
+  req.collection = collection;
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
